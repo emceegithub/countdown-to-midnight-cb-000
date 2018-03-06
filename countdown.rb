47427cf9ac347593573cb1858af6f9d4dccace39
Mark Cruz <markchriscruz@yahoo.com>
2018-03-06 00:28:31 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -8,3 +8,12 @@ def countdown(number)
   end
   return "HAPPY NEW YEAR!"
 end
+
+def countdown_with_sleep(number)
+  while 0 < number
+    puts "#{number} SECOND(S)!"
+    number -= 1
+    sleep 1
+  end
+  return "HAPPY NEW YEAR!"
+end
