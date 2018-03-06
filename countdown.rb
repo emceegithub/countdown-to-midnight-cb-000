377fd101466dfd359c7b1e42f92b8da843a1f160
Mark Cruz <markchriscruz@yahoo.com>
2018-03-06 00:22:41 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,5 +1,9 @@
 #write your code here
 
-def countdown
-  
+def countdown(integer)
+  counter = 0
+  while counter < integer
+    puts "The current number is less than 20."
+    counter += 1
+  end
 end
